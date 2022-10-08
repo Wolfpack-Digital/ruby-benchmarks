8207b66d3fcdb1f511d4317d4c32858293c3a2c5
Victor Motogna <vmotogna@gmail.com>
2022-10-08 18:32:03 +0300

    Update namings to be consistent & add message in main.sh

--- main.sh
+++ main.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 
+echo 'Welcome to ruby-benchmarks.'
+echo
+
+echo '---------------'
+echo 'Running csv parser'
 chmod 755 shell_scripts/csv_parser.sh
 source shell_scripts/csv_parser.sh
+
+echo '---------------'
