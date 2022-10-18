8d2fac3c34e48adf1d05235faec78377747f7615
Victor Motogna <vmotogna@gmail.com>
2022-10-18 20:00:31 +0300

    Add multi-chooser option (#12)

--- main.sh
+++ main.sh
@@ -1,7 +1,30 @@
 #!/usr/bin/env bash
 
+chmod 755 shell_scripts/multi_chooser.sh
+source shell_scripts/multi_chooser.sh
+prompt_for_multiselect result "Image compression; CSV Parser"
+
+list=('Image compression' 'CSV parser')
+
+for i in "${!result[@]}"; do
+  echo ${list[$i]}
+  if [[ ${result[$i]} == "" ]]; then
+    echo 'false'
+  else
+    echo 'true'
+  fi
+  echo '-------'
+  echo
+done
+
 echo 'Welcome to ruby-benchmarks.'
 echo
+
+echo 'Your current setup is:'
+system_profiler SPSoftwareDataType SPHardwareDataType | grep -E '^\s*Memory: (.*)'
+echo $1
+echo '---------------'
+
 
 echo '---------------'
 echo 'Running csv parser'
