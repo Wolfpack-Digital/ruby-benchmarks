befd2b2d31edcefb44508fddbd03f6a148026eb6
flaviuraita <flaviu.raita@wolfpack-digital.com>
2022-10-18 20:35:05 +0300

    Improve rails commands execution

--- ruby_scripts/rails_project/seeds.rb
+++ ruby_scripts/rails_project/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 10000.times do |i|
   TestModel.create!(description: "test model #{i+1}")
 end
--- shell_scripts/rails_project/rails_project.sh
+++ shell_scripts/rails_project/rails_project.sh
@@ -7,13 +7,13 @@ rails new rails_project -d postgresql --api
 cp ../ruby_scripts/rails_project/seeds.rb rails_project/db
 cd rails_project
 
-rails db:create
-rails g model TestModel description:string
-rails db:migrate
+bundle exec rails db:create
+bundle exec rails g model TestModel description:string
+bundle exec rails db:migrate
 bundle install
-rails db:seed
+bundle exec rails db:seed
 
-rails db:drop
+bundle exec rails db:drop
 
 cd ../../
 rm -rf rails_projects
