b3b329b68d26eb65880266882e5a4b1d4dd5a939
Codreanu Ioan-Andrei <50551781+codrutzu@users.noreply.github.com>
2022-10-18 20:25:13 +0300

    feat: options list

--- main.sh
+++ main.sh
@@ -3,14 +3,22 @@
 chmod 755 shell_scripts/multi_chooser.sh
 source shell_scripts/multi_chooser.sh
 
-prompt_for_multiselect result "csv parser;none"
+declare -a options_list=("csv_parser" "none")
 
-if  [[ $result == true ]]; then
-  echo '---------------'
-  echo 'Running csv parser'
-  chmod 755 shell_scripts/csv_parser.sh
-  source shell_scripts/csv_parser.sh
-else
-  echo '---------------'
-  echo 'Thanks for stopping by! See you next time.'
-fi
+function join { local IFS="$1"; shift; echo "$*"; }
+input_options=$(join ";" ${options_list[@]})
+
+prompt_for_multiselect result ${input_options}
+
+for i in ${!options_list[@]}; do
+  if [[ ${options_list[$i]} == "csv_parser" ]] && [[ ${result[$i]} == true ]]; then
+    echo '---------------'
+    echo 'Running csv parser'
+    chmod 755 shell_scripts/csv_parser.sh
+    source shell_scripts/csv_parser.sh
+  elif [[ ${options_list[$i]} == "none" ]] && [[ ${result[$i]} == true ]]; then
+    echo '---------------'
+    echo 'Thanks for stopping by! See you next time.'
+  fi
+done
+
